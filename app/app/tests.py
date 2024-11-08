5a5de514e8987b6984c8c0c00f9856f685408c2e
Azhar Saleem <saleemazhar@gmail.com>
2024-11-08 13:40:36 -0500

    Fix test.py lint issues2

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,7 +2,6 @@
 Sample Test
 """
 from django.test import SimpleTestCase
-
 from app import calc
 
 class CalcTests(SimpleTestCase):
@@ -12,12 +11,11 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers togather."""
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
+    
         
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
-        
         self.assertEqual(res, 5)
         
